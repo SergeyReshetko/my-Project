b1dd8d1505ebe51783c7524a961be098580f702f
ReshetkoSergey <dev.rsergey@gmail.com>
2025-10-28 15:30:45 +0300

    1) Transferring the junit version tag to pom.xml 2) StringUtilTest has placed the declaration of the StringUtil object in the fields of the class 3) deleted the target package from the repository

--- src/test/java/org/example/util/StringUtilTest.java
+++ src/test/java/org/example/util/StringUtilTest.java
@@ -5,48 +5,44 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilTest {
+    private final StringUtil textUtil = new StringUtil();
     
     // Проверяем результат на выполнение функции
     @Test
     void testReverseLetters() {
-        StringUtil textUtils = new StringUtil();
         String testText = "J@va the be$t!123";
-        String result = textUtils.reverseLetters(testText);
+        String result = textUtil.reverseLetters(testText);
         assertEquals("t@eb eht av$J!123", result);
     }
     
     // Проверяем на входную пустую строку
     @Test
     void testReverseLettersEmpty() {
-        StringUtil textUtils = new StringUtil();
         String testText = "";
-        String result = textUtils.reverseLetters(testText);
+        String result = textUtil.reverseLetters(testText);
         assertEquals("", result);
     }
     
     // Проверяем на входной null ожидаем пустую строку
     @Test
     void testReverseLettersNull() {
-        StringUtil textUtils = new StringUtil();
-        String result = textUtils.reverseLetters(null);
+        String result = textUtil.reverseLetters(null);
         assertNotNull(result);
     }
     
     // Проверяем длину входной сроки
     @Test
     void testReverseLettersLength() {
-        StringUtil textUtils = new StringUtil();
         String testText = "J@va the be$t!123";
-        String result = textUtils.reverseLetters(testText);
+        String result = textUtil.reverseLetters(testText);
         assertEquals(testText.length(), result.length());
     }
     
     // Проверяем выполнение метода по последнему буквенному символу входных данных
     @Test
     void testReverseLettersLastSimbol() {
-        StringUtil textUtils = new StringUtil();
         String testText = "J@va the be$t!123";
-        String result = textUtils.reverseLetters(testText);
+        String result = textUtil.reverseLetters(testText);
         char lastLetterText = ' ';
         
         for (int i = testText.length() - 1; i >= 0; i--) {
@@ -56,16 +52,14 @@ class StringUtilTest {
                 break;
             }
         }
-        
         assertEquals(lastLetterText, result.charAt(0));
     }
     
     // Проверяем выполнение метода по первому буквенному символу входных данных
     @Test
     void testReverseLettersFirstSimbol() {
-        StringUtil textUtils = new StringUtil();
         String testText = "J@va the be$t!123";
-        String result = textUtils.reverseLetters(testText);
+        String result = textUtil.reverseLetters(testText);
         int indexLastLetterText = 0;
         
         for (int i = result.length() - 1; i >= 0; i--) {
@@ -75,7 +69,6 @@ class StringUtilTest {
                 break;
             }
         }
-        
         assertEquals(testText.charAt(0), result.charAt(indexLastLetterText));
     }
 }
